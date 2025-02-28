6dbde8f1e52c4886bf579a8e653db71d295e033d
Khushvardhan Bhardwaj <97383093+cryptbird@users.noreply.github.com>
2025-02-28 16:03:09 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <div className=" bg-gradient-to-b from-indigo-300 via-30% via-blue-200 text-center text-2xl font-light p-4">Google Meet <b>Clone</b></div>;
+  return <div className=" bg-gradient-to-b from-indigo-300 via-30% via-blue-200 text-center text-4xl font-light p-4"><img src="/logo.png" style={{height:"80px"}} alt="" /> <b>TCI</b> Meet</div>;
 }
 
 export default Header;
